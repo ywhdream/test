eb2d2c1d6d4ee6e77c90b5a43e752ebdd49f6616
yiyikeji <wenhao.yang@yiartkeji.com>
2024-12-23 17:00:10 +0800

    1、配置

--- chatkit-ui/build.gradle.kts
+++ chatkit-ui/build.gradle.kts
@@ -95,7 +95,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.ywhdream"
                 artifactId = "test"
-                version = "1.0.1"
+                version = "1.0.2"
             }
         }
     }
