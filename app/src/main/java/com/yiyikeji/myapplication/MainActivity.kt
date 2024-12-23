ffbc0e6b1f7cf4e24bf562bc2fc1431cf4eb819e
yiyikeji <wenhao.yang@yiartkeji.com>
2024-12-23 16:30:07 +0800

    Initial commit

---
+++ app/src/main/java/com/yiyikeji/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yiyikeji.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.netease.yunxin.kit.chatkit.ui.avtivity.AVChatActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+      
+    }
+}
