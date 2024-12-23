efa6c4cb51dde73c835a6fd8cf6218d483da7867
yiyikeji <wenhao.yang@yiartkeji.com>
2024-12-23 16:31:53 +0800

    1、配置

--- app/src/main/java/com/yiyikeji/myapplication/MainActivity.kt
+++ app/src/main/java/com/yiyikeji/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-      
+
     }
 }
